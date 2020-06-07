cc2e57d562ff994186d4d148fda3df63cff3b01a
WangPengFeiSCDN <1316129661@qq.com>
2020-06-07 13:36:24 +0800

    将本地项目添加到git

---
+++ gitDemo1/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.itheima.git;
+
+/**
+ * @Description: git的练习类
+ * @Author: pengfei.wang
+ * @CreateDate: 2020-06-07
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
